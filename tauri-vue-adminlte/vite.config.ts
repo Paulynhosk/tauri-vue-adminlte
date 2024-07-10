a35b12eddc1e74abdd21d98eda27a99b09139722
Paulynhosk <paulynhosk@gmail.com>
2024-07-10 02:40:54 -0300

    Update package

--- tauri-vue-adminlte/vite.config.ts
+++ tauri-vue-adminlte/vite.config.ts
@@ -4,4 +4,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  clearScreen: false,
+  server: {
+    port: 5173,
+    strictPort: true,  },
+  envPrefix: ['VITE_', 'TAURI_PLATFORM', 'TAURI_ARCH', 'TAURI_FAMILY', 'TAURI_PLATFORM_VERSION', 'TAURI_PLATFORM_TYPE', 'TAURI_DEBUG'],
+  build: {
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
 })
